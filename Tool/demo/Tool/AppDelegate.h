9255a9d6b8d3c0db3064fc9243d497d5709c38dd
hufei <hufei@nightstation.cn>
2018-05-22 23:52:07 +0800

    test

---
+++ Tool/demo/Tool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tool
+//
+//  Created by 胡斐 on 2018/5/19.
+//  Copyright © 2018年 jackson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tool/demo/Tool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tool
+//
+//  Created by 胡斐 on 2018/5/19.
+//  Copyright © 2018年 jackson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
