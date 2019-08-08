0e1ac801d16ebb949465d59c950d6d5baa284767
iwantFB <13396087879@163.com>
2019-08-08 10:46:54 +0800

    Update HFCircleImageView.h

--- Tool/demo/Tool/class/HFCircleImageView.h
+++ Tool/demo/Tool/class/HFCircleImageView.h
@@ -2,15 +2,17 @@
 //  HFCircleImageView.h
 //  DEMO
 //
-//  Created by 夜站 on 2018/1/19.
-//  Copyright © 2018年 夜站. All rights reserved.
+//  Created by 胡斐 on 2018/1/19.
+//  Copyright © 2018年 胡斐. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 
 @interface HFCircleImageView : UIImageView
 
 /*圆角Size**/
 @property (nonatomic, assign)CGSize cornerSize;
+
+@property (nonatomic, assign) CGFloat boarderWidth;
+@property (nonatomic, strong) UIColor *boarderColor;
 
 @end
