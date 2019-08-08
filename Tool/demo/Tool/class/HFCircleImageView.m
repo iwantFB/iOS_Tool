b0a7176fd0d9627c1cd2add938de14f54b3dbae4
iwantFB <13396087879@163.com>
2019-08-08 10:46:24 +0800

    Update HFCircleImageView.m
    新增头像边框

--- Tool/demo/Tool/class/HFCircleImageView.m
+++ Tool/demo/Tool/class/HFCircleImageView.m
@@ -2,31 +2,25 @@
 //  HFCircleImageView.m
 //  DEMO
 //
-//  Created by 夜站 on 2018/1/19.
-//  Copyright © 2018年 夜站. All rights reserved.
+//  Created by 胡斐 on 2018/1/19.
+//  Copyright © 2018年 胡斐. All rights reserved.
 //
 
 #import "HFCircleImageView.h"
 
 @interface HFCircleImageView()
 
-@property (nonatomic, strong)CAShapeLayer *maskLayer;
+@property (nonatomic, strong) CAShapeLayer *maskLayer;
+@property (nonatomic, strong) CAShapeLayer *boarderLayer;
 
 @end
 
 @implementation HFCircleImageView
 
-//- (void)layoutSubviews
-//{
-//    [super layoutSubviews];
-//    
-//    [self p_updateCircleMask];
-//}
-
 - (void)layoutSublayersOfLayer:(CALayer *)layer
 {
     [super layoutSublayersOfLayer:layer];
-
+    
     CGFloat centerX = CGRectGetMidX(self.bounds);
     CGFloat centerY = CGRectGetMidY(self.bounds);
     CGFloat width = CGRectGetWidth(self.frame);
@@ -38,7 +32,13 @@
         _cornerSize = CGSizeMake(cornerRadius, cornerRadius);
     };
     
-   UIBezierPath *circlePath = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(centerX-cornerRadius, centerY-cornerRadius, cornerRadius*2.0, cornerRadius*2.0) byRoundingCorners:UIRectCornerAllCorners cornerRadii:_cornerSize];
+    UIBezierPath *circlePath = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(centerX-cornerRadius, centerY-cornerRadius, cornerRadius*2.0, cornerRadius*2.0) byRoundingCorners:UIRectCornerAllCorners cornerRadii:_cornerSize];
+    if(_boarderWidth > 0 && _boarderColor){
+        if(!self.boarderLayer.superlayer)[self.layer addSublayer:self.boarderLayer];
+        self.boarderLayer.lineWidth = _boarderWidth;
+        self.boarderLayer.path = circlePath.CGPath;
+        self.boarderLayer.strokeColor = _boarderColor.CGColor;
+    }
     self.maskLayer.path = circlePath.CGPath;
     
     self.layer.mask = self.maskLayer;
@@ -73,5 +73,14 @@
     }
     return _maskLayer;
 }
-@end
 
+- (CAShapeLayer *)boarderLayer
+{
+    if(!_boarderLayer){
+        _boarderLayer = [CAShapeLayer layer];
+        _boarderLayer.fillColor = [UIColor clearColor].CGColor;
+    }
+    return _boarderLayer;
+}
+
+@end
